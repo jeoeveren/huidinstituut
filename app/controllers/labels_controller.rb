dedc95c7c70c3ca4dfb98f4fc26684bce077ea8f
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-07-26 23:37:22 +0200

    Created new POST for pagination

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,23 +1,34 @@
 class LabelsController < ApplicationController
-  before_action :set_offset, only: [:index, :sort_name]
+#before_action :set_offset, only: [:index]
+  before_action :set_offset, except: [:jeroentje]
 
   def index
     @labels = Label.all.limit(@limit).offset((@page*@limit)-@limit+1)
   end
 
-  def sort_name
-    @page = @page + 1
+    def sort_name
+      @labels = Label.sort_name
+      render action: :index
+    end
+
+    def sort_show_order
+      @labels = Label.sort_show_order
+      render action: :index
+    end
+
+    def sort_visible
+      @labels = Label.sort_visible
+      render action: :index
+    end
+
+  def jeroentje
+    # Receive data from index form en get params
+    # params are passed as strings!
+    @page = params[:page].to_i
+    @pages = params[:pages].to_i
+    @limit = params[:limit].to_i
+    @action = params[:action]
     @labels = Label.sort_name.limit(@limit).offset((@page*@limit)-@limit+1)
-    render action: :index
-  end
-
-  def sort_show_order
-    @labels = Label.sort_show_order
-    render action: :index
-  end
-
-  def sort_visible
-    @labels = Label.sort_visible
     render action: :index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   get "categories" => "categories#index"
-  get "labels" => "labels#index"
+  # get "labels" => "labels#index"
+  post "labels" => "labels#index"
   get "products" => "products#index"
   get "treatments" => "treatments#index"
   root to: 'pages#home'
 
-  resources :labels do
+  resources :labels, only: :index do
     collection do
-      get :sort_name
-      get :sort_show_order
-      get :sort_visible
+      get  :sort_name
+      get  :sort_show_order
+      get  :sort_visible
+      post :jeroentje
     end
   end
+
 
 end
